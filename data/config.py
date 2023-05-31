fbda04431c42b80e8b1a55fa34deb18e2ed8144b
SupperBest17 <sherliyevakbarjon@gmail.com>
2023-05-31 19:52:46 +0500

    Config O'zgardi

--- data/config.py
+++ data/config.py
@@ -1,10 +1,5 @@
-from environs import Env
-
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = "5880077443:AAFe-pKJJJuF88EeKDGaqX1tWM3-7CJqW3c"
+ADMINS = [1082310069,]
+IP = "localhost"
